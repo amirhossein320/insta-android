849700c50e7183d136543b9686fd3d10d43cb2d9
MrEleven <mr.amirprogrammer@gmail.com>
2022-04-16 19:07:22 +0430

    support multi dex update dependencies application class with di

---
+++ app/src/main/java/ir/amir/isnta/App.kt
@@ -0,0 +1,8 @@
+package ir.amir.isnta
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : MultiDexApplication() {
+}
--- app/src/main/java/ir/amir/isnta/presenter/main/MainActivity.kt
+++ app/src/main/java/ir/amir/isnta/presenter/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ir.amir.isnta
+package ir.amir.isnta.presenter.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.amir.isnta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
